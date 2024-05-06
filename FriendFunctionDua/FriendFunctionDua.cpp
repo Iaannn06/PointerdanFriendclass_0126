fbaa43fe0e10af64319f420c39cd29489bc4f888
Iaannn06 <alfianazad05@gmail.com>
2024-05-06 11:11:16 +0700

    membuat class untuk nilai pelajar

---
+++ FriendFunctionDua/FriendFunctionDua.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+#include <iostream>
+
+class pelajar;
+class manusia
+{
+public:
+    void showNilaiPelajar(pelajar& x);
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
