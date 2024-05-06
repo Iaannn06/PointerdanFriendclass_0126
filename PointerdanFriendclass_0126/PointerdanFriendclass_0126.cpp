50c8222e1b39a92a0e807f5c852267878586a73e
Iaannn06 <alfianazad05@gmail.com>
2024-05-06 10:30:20 +0700

    Add project files.

---
+++ PointerdanFriendclass_0126/PointerdanFriendclass_0126.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+#include <iostream>
+#include <string>;
+
+class mahasiswa {
+public:
+    int nim;
+    void showNim() {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+int main()
+{
+    
+}
+
